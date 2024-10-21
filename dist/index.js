4147f990f7456b3698c8e52f1b0d5dcbcfaf73bf
mschmid03 <m.schmid@studio-03.de>
2024-10-21 15:33:50 +0200

    ready for production

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 "use strict";
 (() => {
   // sliderListener.ts
-  function setSliderListener(sliderNav2, sliderContentItems2) {
-    const sliderItems = sliderNav2?.querySelectorAll(".linsenwelt-slider_item");
+  function setSliderListener(sliderNav, sliderContentItems) {
+    const sliderItems = sliderNav?.querySelectorAll(".linsenwelt-slider_item");
     sliderItems?.forEach((item, i) => {
       const itemWidth = item.scrollWidth;
       const itemClone = item.cloneNode(true);
@@ -13,17 +13,17 @@
       itemClone?.addEventListener("click", () => {
         itemClone.classList.add("active");
         console.log(itemElement);
-        if (sliderContentItems2) {
-          sliderContentItems2[i].classList.add("active");
-          if (sliderNav2) {
-            sliderNav2.style.transform = `translateX(calc((${i} * (-${itemWidth}px - 1.25rem)`;
-            const newSliderItems = sliderNav2?.querySelectorAll(
+        if (sliderContentItems) {
+          sliderContentItems[i].classList.add("active");
+          if (sliderNav) {
+            sliderNav.style.transform = `translateX(calc((${i} * (-${itemWidth}px - 1.25rem)`;
+            const newSliderItems = sliderNav?.querySelectorAll(
               ".linsenwelt-slider_item"
             );
             newSliderItems?.forEach((item2, j) => {
               if (j != i) {
                 item2.classList.remove("active");
-                sliderContentItems2[j].classList.remove("active");
+                sliderContentItems[j].classList.remove("active");
               }
             });
           }
@@ -46,18 +46,20 @@
   }
 
   // index.ts
-  var sliderNav = document.getElementById("linsenwelt-nav-wrapper");
-  var sliderContent = document.getElementById("linsenwelt-content-wrapper");
-  var sliderContentItems = sliderContent?.querySelectorAll(
-    ".linsenwelt-slider_content-item"
-  );
-  sliderContentItems.forEach((item) => {
-    item.classList.remove("visible");
-  });
-  setSliderListener(sliderNav, sliderContentItems);
-  document.addEventListener("resize", () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const sliderNav = document.getElementById("linsenwelt-nav-wrapper");
+    const sliderContent = document.getElementById("linsenwelt-content-wrapper");
+    const sliderContentItems = sliderContent?.querySelectorAll(
+      ".linsenwelt-slider_content-item"
+    );
+    sliderContentItems.forEach((item) => {
+      item.classList.remove("visible");
+    });
     setSliderListener(sliderNav, sliderContentItems);
+    document.addEventListener("resize", () => {
+      setSliderListener(sliderNav, sliderContentItems);
+    });
+    centerSticky("sticky-wrapper-abstimmung");
   });
-  centerSticky("sticky-wrapper-abstimmung");
 })();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
 import { setSliderListener } from "./sliderListener";
 import { centerSticky } from "./centerSticky";
 
-const sliderNav = document.getElementById("linsenwelt-nav-wrapper");
-const sliderContent = document.getElementById("linsenwelt-content-wrapper");
-const sliderContentItems = sliderContent?.querySelectorAll(
-  ".linsenwelt-slider_content-item",
-) as NodeListOf<Element>;
-sliderContentItems.forEach((item) => {
-  item.classList.remove("visible");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const sliderNav = document.getElementById("linsenwelt-nav-wrapper");
+  const sliderContent = document.getElementById("linsenwelt-content-wrapper");
+  const sliderContentItems = sliderContent?.querySelectorAll(
+    ".linsenwelt-slider_content-item",
+  ) as NodeListOf<Element>;
+  sliderContentItems.forEach((item) => {
+    item.classList.remove("visible");
+  });
 
-setSliderListener(sliderNav, sliderContentItems);
-document.addEventListener("resize", () => {
   setSliderListener(sliderNav, sliderContentItems);
+  document.addEventListener("resize", () => {
+    setSliderListener(sliderNav, sliderContentItems);
+  });
+  centerSticky("sticky-wrapper-abstimmung");
 });
-centerSticky("sticky-wrapper-abstimmung");
