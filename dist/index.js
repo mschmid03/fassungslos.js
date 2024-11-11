8773a32507d27a5c5b35a70ca02262f04044d9ff
mschmid03 <m.schmid@studio-03.de>
2024-11-11 14:42:02 +0100

    Changes

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,68 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sliderListener_1 = require("./sliderListener");
-const centerSticky_1 = require("./centerSticky");
-document.addEventListener("DOMContentLoaded", () => {
+(() => {
+  // sliderListener.ts
+  function setSliderListener(sliderNav, sliderContentItems) {
+    const sliderItems = sliderNav?.querySelectorAll(".linsenwelt-slider_item");
+    sliderItems?.forEach((item, i) => {
+      const itemWidth = item.scrollWidth;
+      const itemClone = item.cloneNode(true);
+      console.log(itemWidth);
+      console.log(itemClone);
+      const itemElement = item.parentNode?.replaceChild(itemClone, item);
+      console.log(itemClone);
+      itemClone?.addEventListener("click", () => {
+        itemClone.classList.add("active");
+        console.log(itemElement);
+        if (sliderContentItems) {
+          sliderContentItems[i].classList.add("active");
+          if (sliderNav) {
+            sliderNav.style.transform = `translateX(calc((${i} * (-${itemWidth}px - 1.25rem)`;
+            const newSliderItems = sliderNav?.querySelectorAll(
+              ".linsenwelt-slider_item"
+            );
+            newSliderItems?.forEach((item2, j) => {
+              if (j != i) {
+                item2.classList.remove("active");
+                sliderContentItems[j].classList.remove("active");
+              }
+            });
+          }
+        }
+      });
+    });
+  }
+
+  // centerSticky.ts
+  function centerSticky(id) {
+    const stickyDiv = document.getElementById(id);
+    let height = stickyDiv.offsetHeight;
+    stickyDiv.style.marginTop = `${height / 2}px`;
+    stickyDiv.style.marginBottom = `-${height / 2}px`;
+    window.addEventListener("resize", () => {
+      height = stickyDiv.offsetHeight;
+      stickyDiv.style.marginTop = `${height / 2}px`;
+      stickyDiv.style.marginBottom = `-${height / 2}px`;
+    });
+  }
+
+  // index.ts
+  document.addEventListener("DOMContentLoaded", () => {
     const sliderNav = document.getElementById("linsenwelt-nav-wrapper");
     const sliderContent = document.getElementById("linsenwelt-content-wrapper");
-    const sliderContentItems = sliderContent === null || sliderContent === void 0 ? void 0 : sliderContent.querySelectorAll(".linsenwelt-slider_content-item");
+    const sliderContentItems = sliderContent?.querySelectorAll(
+      ".linsenwelt-slider_content-item"
+    );
     sliderContentItems.forEach((item) => {
-        item.classList.remove("visible");
-        console.log(item.classList);
+      item.classList.remove("visible");
+      console.log(item.classList);
     });
-    (0, sliderListener_1.setSliderListener)(sliderNav, sliderContentItems);
+    setSliderListener(sliderNav, sliderContentItems);
     document.addEventListener("resize", () => {
-        (0, sliderListener_1.setSliderListener)(sliderNav, sliderContentItems);
+      setSliderListener(sliderNav, sliderContentItems);
     });
     if (document.documentElement.clientWidth > 991) {
-        (0, centerSticky_1.centerSticky)("sticky-wrapper-abstimmung");
+      centerSticky("sticky-wrapper-abstimmung");
     }
-});
+  });
+})();
+//# sourceMappingURL=index.js.map
