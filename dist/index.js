a7345446c13eb276987f37c6fddbf29da6ae5f29
mschmid03 <m.schmid@studio-03.de>
2024-11-11 14:37:16 +0100

    Changes

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var sliderListener_1 = require("./sliderListener");
-var centerSticky_1 = require("./centerSticky");
-document.addEventListener("DOMContentLoaded", function () {
-    var sliderNav = document.getElementById("linsenwelt-nav-wrapper");
-    var sliderContent = document.getElementById("linsenwelt-content-wrapper");
-    var sliderContentItems = sliderContent === null || sliderContent === void 0 ? void 0 : sliderContent.querySelectorAll(".linsenwelt-slider_content-item");
-    sliderContentItems.forEach(function (item) {
+const sliderListener_1 = require("./sliderListener");
+const centerSticky_1 = require("./centerSticky");
+document.addEventListener("DOMContentLoaded", () => {
+    const sliderNav = document.getElementById("linsenwelt-nav-wrapper");
+    const sliderContent = document.getElementById("linsenwelt-content-wrapper");
+    const sliderContentItems = sliderContent === null || sliderContent === void 0 ? void 0 : sliderContent.querySelectorAll(".linsenwelt-slider_content-item");
+    sliderContentItems.forEach((item) => {
         item.classList.remove("visible");
         console.log(item.classList);
     });
     (0, sliderListener_1.setSliderListener)(sliderNav, sliderContentItems);
-    document.addEventListener("resize", function () {
+    document.addEventListener("resize", () => {
         (0, sliderListener_1.setSliderListener)(sliderNav, sliderContentItems);
     });
     if (document.documentElement.clientWidth > 991) {
